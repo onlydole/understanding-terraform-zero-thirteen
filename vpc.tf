fa419d587164973ecd383b8eefe24776dc2e6500
Taylor Dolezal <onlydole@gmail.com>
2020-08-18 19:49:42 -0700

    Remove tags, enable DNS hostnames

--- vpc.tf
+++ vpc.tf
@@ -9,11 +9,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-
-  vpc_tags = {
-    "kubernetes.io/cluster/${var.project_name}" = "shared"
-  }
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb"                    = 1
