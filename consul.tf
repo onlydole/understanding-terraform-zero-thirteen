a521c4b3675f9adfbe2a39639684f76716b15d43
Taylor Dolezal <onlydole@gmail.com>
2020-08-18 17:17:41 -0700

    Remove Consul module

--- consul.tf
+++
@@ -1,7 +0,0 @@
-module "consul" {
-  source       = "hashicorp/consul/aws"
-  version      = "0.7.7"
-  vpc_id       = module.vpc.vpc_id
-  cluster_name = var.project_name
-  ssh_key_name = aws_key_pair.zero_thirteen.key_name
-}
